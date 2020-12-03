a804f25edb3001eb5d7d4044ca704040424cdf4b
mik0war <anton.korneev.2011@yandex,ru>
2020-12-03 07:42:41 +0300

    Initial commit

---
+++ src/main/java/TravelingSalesman.java
@@ -0,0 +1,40 @@
+import io.jenetics.*;
+import io.jenetics.engine.*;
+
+public class TravelingSalesman {
+    private static final double[][] MATRIX = new double[][]{
+            {100, 1, 3, 4, 5},
+            {1, 100, 1, 4, 8},
+            {3, 1, 100, 5, 1},
+            {4, 4, 5, 100, 2},
+            {5, 8, 1, 2, 100}
+    };
+
+    private static double eval(final int[] var) {
+        double sum = 0.0;
+
+        for (int i = 0; i < var.length; i++){
+            sum += MATRIX[var[i]][var[(i+1) % var.length]];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+
+        final Engine<EnumGene<Integer>, Double> engine = Engine
+                .builder(
+                        TravelingSalesman::eval,
+                        Codecs.ofPermutation(MATRIX.length))
+			    .optimize(Optimize.MINIMUM)
+                .maximalPhenotypeAge(10)
+                .populationSize(500)
+                .build();
+
+        final Phenotype<EnumGene<Integer>, Double> best =
+                engine.stream()
+                        .limit(100)
+                        .collect(EvolutionResult.toBestPhenotype());
+
+        System.out.println(best);
+    }
+}
